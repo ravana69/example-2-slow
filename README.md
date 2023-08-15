e30b277d7c87fe4df680a8c10c07da0085786ee2
Ravana ⭐ <47528708+ravana69@users.noreply.github.com>
2023-08-15 22:04:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Example #2 (slow)
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/sfi0zy/pen/QRLrBJ](https://codepen.io/sfi0zy/pen/QRLrBJ).
+
---
+++ src/script.js
@@ -0,0 +1,40 @@
+let CANVAS = document.getElementById('noisy-canvas'),
+    CTX    = CANVAS.getContext('2d');
+
+
+main();
+
+
+function main() {
+    window.addEventListener('resize', updateCanvasSize);
+    updateCanvasSize();
+    render();
+}
+
+
+function getRandom() {
+    return Math.random() * 255;
+}
+
+
+function render() {
+    let imageData = CTX.createImageData(CTX.canvas.width, CTX.canvas.height);
+
+    for (let i = 0; i < imageData.data.length; i += 4) {
+        const color = getRandom();
+        imageData.data[i]     = color;
+        imageData.data[i + 1] = color;
+        imageData.data[i + 2] = color;
+        imageData.data[i + 3] = 255;
+    }
+
+    CTX.putImageData(imageData, 0, 0);
+
+    requestAnimationFrame(render);
+}
+
+
+function updateCanvasSize() {
+    CTX.canvas.height = window.innerHeight;
+    CTX.canvas.width  = window.innerWidth;
+}
